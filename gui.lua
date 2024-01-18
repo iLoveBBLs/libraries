9c8a5a3e5f66ab9268bc749d510e1a5e903aaf4f
iLoveBBLs <138410964+iLoveBBLs@users.noreply.github.com>
2024-01-18 13:43:43 -0800

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,8 +1,29 @@
+-- VARIABLES NEEDED
+local function service(...) return game:GetService(...) end
+local Players = service("Players")
+local MarketplaceService = service("MarketplaceService")
+local ReplicatedStorage = service("ReplicatedStorage")
+local HttpService = service("HttpService")
+local Constants = require(ReplicatedStorage:WaitForChild("Constants"))
+local Connection = ReplicatedStorage:WaitForChild("Connection")
+local ConnectionEvent = ReplicatedStorage:WaitForChild("ConnectionEvent")
+local function getservers()
+	return Connection:InvokeServer(399)
+end
+local function joinserver(instid)
+	return Connection:InvokeServer(400,instid)
+end
+-- END OF VARIABLES
+
+-- GUI VARIABLES
 local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")()
+local win = lib:Window("MEEPCITY",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
+local tab = win:Tab("Avatar")
+-- END OF GUI VARIABLES
 
-local win = lib:Window("PREVIEW",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
-
-local tab = win:Tab("Tab 1")
+tab:Textbox("Textbox",true, function(t)
+print(t)
+end)
 
 tab:Button("Button", function()
 lib:Notification("Notification", "Hello!", "Hi!")
@@ -12,30 +33,10 @@ tab:Toggle("Toggle",false, function(t)
 print(t)
 end)
 
-tab:Slider("Slider",0,100,30, function(t)
-print(t)
-end)
-
 tab:Dropdown("Dropdown",{"Option 1","Option 2","Option 3","Option 4","Option 5"}, function(t)
-print(t)
-end)
-
-tab:Colorpicker("Colorpicker",Color3.fromRGB(255,0,0), function(t)
 print(t)
 end)
 
 tab:Textbox("Textbox",true, function(t)
 print(t)
-end)
-
-tab:Bind("Bind",Enum.KeyCode.RightShift, function()
-print("Pressed!")
-end)
-
-tab:Label("Label")
-
-local changeclr = win:Tab("Change UI Color")
-
-changeclr:Colorpicker("Change UI Color",Color3.fromRGB(44, 120, 224), function(t)
-lib:ChangePresetColor(Color3.fromRGB(t.R * 255, t.G * 255, t.B * 255))
 end)
