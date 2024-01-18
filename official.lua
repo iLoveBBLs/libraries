93211cb901307ff3e0dff7e51d3d3598d010a125
iLoveBBLs <138410964+iLoveBBLs@users.noreply.github.com>
2024-01-18 15:14:10 -0800

    Update official.lua

--- official.lua
+++ official.lua
@@ -6,15 +6,19 @@ local HttpService = service("HttpService")
 local Constants = require(ReplicatedStorage:WaitForChild("Constants"))
 local Connection = ReplicatedStorage:WaitForChild("Connection")
 local ConnectionEvent = ReplicatedStorage:WaitForChild("ConnectionEvent")
+
 local function getservers()
 	return Connection:InvokeServer(399)
 end
+
 local function joinserver(instid)
 	return Connection:InvokeServer(400,instid)
 end
+
 local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")()
 local win = lib:Window("MEEPCITY",Color3.fromRGB(44, 120,s 224), Enum.KeyCode.RightControl)
 local tab1 = win:Tab("Avatar")
+
 local function colorToTable(clr) return {tostring(clr.R*255),tostring(clr.G*255),tostring(clr.B*255)} end
 local function ExtractData(humdes)
 	local ava = {}
@@ -47,18 +51,21 @@ local function ExtractData(humdes)
 	ava.AccessoryBlob = layered
 	return ava
 end
+
 tab1:Textbox("Morph into user:",true, function(username)
 	if username and Players:GetUserIdFromNameAsync(username) then
 		local data = ExtractData(Players:GetHumanoidDescriptionFromUserId(Players:GetUserIdFromNameAsync(username)))
 		ConnectionEvent:FireServer(315,data,true)
 	end
 end)
+
 tab1:Textbox("Morph into userid:",true, function(userid)
 	if userid and tonumber(userid) and Players:GetHumanoidDescriptionFromUserId(tonumber(userid)) then
 		local data = ExtractData(Players:GetHumanoidDescriptionFromUserId(tonumber(userid)))
 		ConnectionEvent:FireServer(315,data,true)
 	end
 end)
+
 tab1:Toggle("Unlimited Outfits",false, function(b)
 	if b then
 		Constants.STATS.MAXAvatarEditorCustomOutfits = 999999
