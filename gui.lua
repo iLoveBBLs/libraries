151b602aaae366a88cd4ace01a0e1ff268da6e8e
iLoveBBLs <138410964+iLoveBBLs@users.noreply.github.com>
2024-01-18 13:45:50 -0800

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -21,8 +21,55 @@ local win = lib:Window("MEEPCITY",Color3.fromRGB(44, 120, 224), Enum.KeyCode.Rig
 local tab = win:Tab("Avatar")
 -- END OF GUI VARIABLES
 
-tab:Textbox("Textbox",true, function(t)
-print(t)
+local function colorToTable(clr) return {tostring(clr.R*255),tostring(clr.G*255),tostring(clr.B*255)} end
+
+local function ExtractData(humdes)
+	local ava = {}
+
+	for _,v in pairs({"WidthScale", "HeadScale","HeightScale","DepthScale","BodyTypeScale","ProportionScale"}) do
+		ava[v] = humdes[v]
+	end
+
+	for _,v in pairs({"Face","Head","LeftArm","RightArm","LeftLeg","RightLeg","Torso"}) do
+		ava[v] = humdes[v]
+	end
+
+	for _,v in pairs({"HeadColor","LeftArmColor","RightArmColor","LeftLegColor","RightLegColor","TorsoColor"}) do
+		ava[v] = colorToTable(humdes[v])
+	end
+
+	for _,v in pairs({"GraphicTShirt","Shirt","Pants"}) do
+		ava[v] = humdes[v]
+	end
+
+	for _,v in pairs({"ClimbAnimation","FallAnimation","IdleAnimation","JumpAnimation","RunAnimation","SwimAnimation","WalkAnimation"}) do
+		ava[v] = humdes[v]
+	end
+
+
+	for _,v in pairs({"Hat","Hair","Back","Face","Front","Neck","Shoulders","Waist"}) do
+		ava[v .. "Accessory"] = humdes[v .. "Accessory"]
+	end
+	ava.Emotes = humdes:GetEmotes()
+
+	local layered = humdes:GetAccessories(false)
+
+	for i,v in pairs(layered) do
+		if v.AccessoryType and typeof(v.AccessoryType) == "EnumItem" then
+			v.AccessoryType = v.AccessoryType.Name
+		end
+	end
+
+	ava.AccessoryBlob = layered
+
+	return ava
+end
+
+tab:Textbox("USERNAME TO MORPH",true, function(t)
+	if username and Players:GetUserIdFromNameAsync(username) then
+		local data = ExtractData(Players:GetHumanoidDescriptionFromUserId(Players:GetUserIdFromNameAsync(username)))
+		ConnectionEvent:FireServer(315,data,true)
+	end
 end)
 
 tab:Button("Button", function()
