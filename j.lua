f7c567f1166389df9ffbd7f53d1e23e45b79ddd0
iLoveBBLs <138410964+iLoveBBLs@users.noreply.github.com>
2024-01-18 17:07:09 -0800

    Update j.lua

--- j.lua
+++ j.lua
@@ -1,22 +1,163 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Fuzki-UI-Library/main/FuzkiLibrary.lua"))() -- Gets the Library
-local Main = Library:Create("Fuzki", "Baseplate") -- Creates Main Window | Fuzki (Hub Name) | Baseplate (Game Name)
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Rain-Design/Libraries/main/Shaman/Library.lua'))()
+local Flags = Library.Flags
 
-local Other = Main:CreateSection("Other") | Creates section with name Other
-local Preview = Main:CreateSection("Preview") [size=large]Creates another section with name Preview[/size]
+local Window = Library:Window({
+    Text = "Baseplate"
+})
 
-Preview:CreateLabel("Text Label") -- Text Label with text inside of " "
-Preview:CreateButton("Button Text", "Button Info", function() -- creates button (CreateButton)
-   print("Wow, printed")
-end)
-Preview:CreateToggle("Toggle Info", function(state) -- creates Toggle
-   print(state) -- prints state (true or false)
-end)
-Preview:CreateBind("Keybind Info", Enum.KeyCode.F, function() -- Enum.KeyCode.F is starting KeyBind
-   print("Keybind Was Pressed")
-end)
-Preview:CreateTextBox("TextBox Info", "PlaceHolder", function(v) -- v= gets textbox text after enter pressed
-print(v)
-end)
-Preview:CreateSlider(16, 500, "Slider Info", function(val) -- 16 (Min value) | 500 (Max value)
-print(val) -- prints value 
-end)
+local Tab = Window:Tab({
+    Text = "Aiming"
+})
+
+local Tab2 = Window:Tab({
+    Text = "Visual"
+})
+
+local ChamsSection = Tab2:Section({
+    Text = "Chams"
+})
+
+local Section = Tab:Section({
+    Text = "Aimbot"
+})
+
+local Section2 = Tab:Section({
+    Text = "FOV"
+})
+
+local Section3 = Tab:Section({
+    Text = "Misc",
+    Side = "Right"
+})
+
+ChamsSection:Toggle({
+    Text = "Enabled"
+})
+
+ChamsSection:Toggle({
+    Text = "Color"
+})
+
+ChamsSection:Toggle({
+    Text = "Filled"
+})
+
+ChamsSection:Toggle({
+    Text = "Team Check"
+})
+
+Section:Toggle({
+    Text = "Enabled"
+})
+
+Section:Toggle({
+    Text = "Wall Check"
+})
+
+Section:Toggle({
+    Text = "Smooth Aimbot"
+})
+
+Section2:Toggle({
+    Text = "Enabled"
+})
+
+Section2:Toggle({
+    Text = "Filled FOV"
+})
+
+Section2:Toggle({
+    Text = "FOV Transparency",
+    Tooltip = "Changes your fov transparency."
+})
+
+Section2:Button({
+    Text = "Reset FOV",
+    Tooltip = "This resets your aimbot fov."
+})
+
+Section3:Toggle({
+    Text = "Infinite Ammo"
+})
+
+Section3:Toggle({
+    Text = "No Spread"
+})
+
+Section3:Toggle({
+    Text = "No Bullet Drop",
+    Default = true
+})
+
+Section3:Toggle({
+    Text = "Full Auto"
+})
+
+local a = Section3:Toggle({
+    Text = "No Recoil"
+})
+
+local label = Section3:Label({
+    Text = "This is a label.",
+    Color = Color3.fromRGB(217, 97, 99),
+    Tooltip = "This is a label."
+})
+
+local dropdown = Section:Dropdown({
+    Text = "Dropdown",
+    List = {"Head","Torso","Random"},
+    Flag = "Choosen",
+    Callback = function(v)
+        warn(v)
+    end
+})
+
+Section:RadioButton({
+    Text = "RadioButton",
+    Options = {"Legit","Blatant"},
+    Callback = function(v)
+        warn(v)
+    end
+})
+
+Section:Toggle({
+    Text = "Silent Aimbot"
+})
+
+Section:Input({
+    Placeholder = "Webhook URL",
+    Flag = "URL"
+})
+
+Section:Keybind({
+    Default = Enum.KeyCode.E,
+    Text = "Aimbot Key",
+    Callback = function()
+        warn("Pressed")
+    end
+})
+
+Section:Slider({
+    Text = "Slider Test",
+    Default = 5,
+    Minimum = 0,
+    Maximum = 50,
+    Flag = "SliderFlag",
+    Callback = function(v)
+        warn(v)
+    end
+})
+
+Tab:Select()
+
+wait(5)
+
+--[[dropdown:Refresh({
+    List = {"Head", "Feet"}
+})--]]
+
+label:Set({
+    Text = "This is a red label."
+})
+
+a:Set(true)
